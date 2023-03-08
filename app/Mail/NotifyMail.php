6e1d805ec9ae1e81c572fb5fdf40ecb54e4bb4d5
Allservix <allservix@gmail.com>
2023-03-08 12:54:28 +0100

    AL-70 Mail Petition

--- app/Mail/NotifyMail.php
+++ app/Mail/NotifyMail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class NotifyMail extends Mailable
+class NotifyMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
